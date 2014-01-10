1ae4ba5b943574e8dd7706e888bc1a57aa485c2e
fsi206914 <liang.xjtu@gmail.com>
2014-01-09 21:29:23 -0600

    SkyMain debugging

---
+++ src/ProbSkyline/SkyMain.java
@@ -0,0 +1,93 @@
+package ProbSkyline;
+import mapreduce.ClusterConfig;
+import ProbSkyline.DataStructures.instance;
+import ProbSkyline.DataStructures.item;
+
+import ProbSkyline.ProbSkyQuery.Prune1And2;
+import ProbSkyline.ProbSkyQuery.Prune3;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SkyMain{
+
+	ClusterConfig CC;
+	List<item> itemList;
+
+	public SkyMain(ClusterConfig CC){
+		this.CC= CC;	
+	}
+
+	public instance stringToInstance(String instString){
+
+		String [] div = instString.split(" ");
+		instance inst = null;
+		if(div.length == CC.dim+3){
+			inst= new instance(Util.getObjectID(div[0]), Util.getInstID(div[1]), Util.getProb(div[div.length-1]), CC.dim);
+			inst.setPoint(Util.getPoint(div, CC.dim));
+		}
+		else
+			System.out.println("Sth Wrong in creating instance.");
+
+		return inst;
+	}
+
+
+	public void getItemList(String inputFile){
+
+		SkyClient client = new SkyClient(ClusterConfig.getInstance()); 
+		HashMap<Integer, item> aMap = new HashMap<Integer, item>();
+
+		try{
+		BufferedReader br = new BufferedReader(new FileReader(inputFile));
+		String line = br.readLine();
+		while( line != null){
+
+			instance curr = client.stringToInstance(line);
+			int objectID = curr.objectID;
+			if(aMap.containsKey(objectID) == false){
+
+				item aItem = new item(objectID);              
+				aMap.put(objectID, aItem);
+			}
+			item currItem = aMap.get(objectID);
+			currItem.addInstance(curr);
+			line = br.readLine();
+		}
+		}
+		catch(IOException ioe){
+			ioe.printStackTrace();	
+		}
+		this.itemList = new ArrayList<item>(aMap.values());
+	}
+
+
+
+	public void prune(List<item> itemList){
+		Prune1And2 P12 = new Prune1And2(itemList, CC);	
+		P12.prune();
+		List<instance> afterPrune12List = P12.itemsToinstances();
+
+		Prune3 P3 = new Prune3(afterPrune12List, CC);
+		P3.setClusterConfig(CC);
+		P3.prune();
+	}
+
+
+	public static void main(String [] args){
+
+		SkyMain main = new SkyMain(ClusterConfig.getInstance());        
+		main.getItemList("4.txt");
+		if(main.itemList != null)
+			main.prune(main.itemList);
+		else
+			System.out.println("Sth Wrong in retrieving itemList in getItemList");
+	}
+}
