eebed823516be48be88655e9ef9b255704a1bf3e
fsi206914 <liang.xjtu@gmail.com>
2014-03-12 23:54:29 -0500

    drawing doing

--- trySbt/src/main/scala/ScalaDraw/Draw.scala
+++ trySbt/src/main/scala/ScalaDraw/Draw.scala
@@ -9,43 +9,38 @@ import scala.swing.Panel
 
 case class Dart(val x: Int, val y: Int, val color: java.awt.Color)
 
-class Canvas extends Panel {
-  var centerColor = Color.yellow
-  
-  var darts = List[Dart]()
+class SkyGraph(val w: Double, val h: Double) extends Panel {
 
-  override def paintComponent(g: Graphics2D) {
-    
-    // Start by erasing this Canvas
-    g.clearRect(0, 0, size.width, size.height)
-    
-    // Draw background here
-    g.setColor(Color.blue)
-    g.fillOval(0, 0, 100, 100)
-    g.setColor(Color.red)
-    g.fillOval(20, 20, 60, 60)
-    g.setColor(centerColor)
-    g.fillOval(40, 40, 20, 20)
-    
-    // Draw things that change on top of background
-    for (dart <- darts) {
-      g.setColor(dart.color)
-      g.fillOval(dart.x, dart.y, 10, 10)
-    }
+  val PAD = 20.0
+
+  override def paintComponent(g2: Graphics2D) {
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                        RenderingHints.VALUE_ANTIALIAS_ON);
+
+    // Draw ordinate.
+    g2.draw(new Line2D.Double(PAD, PAD, PAD, h-PAD));
+    g2.draw(new Line2D.Double(PAD, h-PAD, w-PAD, h-PAD));
+
+    var font = g2.getFont()
+    var frc = g2.getFontRenderContext()
+    var lm = font.getLineMetrics("0", frc)
+    var sh = lm.getAscent() + lm.getDescent()
+    val s = "y axis"
+    var sy = PAD + ((h - 2*PAD) - s.length()*sh)/2 + lm.getAscent()
+
+
   }
 
   /** Add a "dart" to list of things to display */
-  def throwDart(dart: Dart) {
-    darts = darts :+ dart
-    // Tell Scala that the display should be repainted
-    repaint()
-  }
 }
 
 object HelloWorld extends SimpleSwingApplication {
+
+	var SIZE = new Dimension()
+	SIZE.setSize(420.0, 420.0)
 	def top = new MainFrame {
 		title = "Hello, World!"
-		contents = new Canvas()
-		size = new Dimension(300,300)
+		contents = new SkyGraph(420.0, 420.0)
+		size = SIZE
 	}
 }
